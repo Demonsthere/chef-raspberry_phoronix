2e53d338f238fbaed928b80a2108c6184a898514
Jakub Blaszczyk | hybris <jakub.blaszczyk@hybris.com>
2016-11-05 21:20:03 +0100

    Setting default suites

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,5 +2,5 @@ default[:raspberry_phoronix][:set_sources] = false
 default[:raspberry_phoronix][:user] = 'vagrant'
 default[:raspberry_phoronix][:packages] = %w(wget unzip php5-cli php-fpdf htop)
 default[:raspberry_phoronix][:pts_url] = 'https://github.com/phoronix-test-suite/phoronix-test-suite/archive/master.zip'
-default[:raspberry_phoronix][:pts_suites] = %w(build-php build-apache)
+default[:raspberry_phoronix][:pts_suites] = %w(build-php build-imagemagick)
 default[:raspberry_phoronix][:pts_home] = "/home/#{node[:raspberry_phoronix][:user]}/.phoronix-test-suite"
